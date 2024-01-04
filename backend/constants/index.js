507024382c64f4330299ea141512fab67cd44164
Anshul Borde <89180266+AnshB0712@users.noreply.github.com>
2024-01-04 16:19:32 +0530

    Removed MongoURI

--- backend/constants/index.js
+++ backend/constants/index.js
@@ -2,8 +2,7 @@ const TOKEN_SECRET = "124jbsh7y7ysbushushsuh878";
 const ACCESS_TOKEN_EXP = "10s";
 const REFRESH_SECRET = "124jbsh7y7ysbushushsuh877";
 const REFRESH_TOKEN_EXP = "60s";
-const MONGO_URI =
-  "mongodb+srv://ansh:ansh@cluster0.kimulss.mongodb.net/?retryWrites=true&w=majority";
+const MONGO_URI = "";
 
 module.exports = {
   TOKEN_SECRET,
